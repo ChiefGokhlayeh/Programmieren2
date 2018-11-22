9b41bd5c07a72d225569d71ffe14a40fd120c548
Andreas Baulig <andreas.baulig@gmail.com>
2018-11-22 09:57:17 +0100

    Replace visual studio with makefile project

--- Aufgabe1_1/Main.c
+++ Aufgabe1_1/Main.c
@@ -3,33 +3,33 @@
 int main(void) {
 	char outputType = '\0';
 	int number = 0;
+	char string[128];
 
 	printf("Wollen Sie eine (O)ktal-, eine He(x)adezimal- oder eine (D)ezimal-Zahl eingeben: ");
-	scanf_s("%c", &outputType);
-	getchar();
+	fgets(string, sizeof(string), stdin);
+	sscanf(string, "%c", &outputType);
 
 	switch (outputType) {
 		case 'o':
 		case 'O':
 			printf("Oktal-Zahl eingeben: ");
-			scanf_s("%o", &number);
-			getchar();
+			fgets(string, sizeof(string), stdin);
+			sscanf(string, "%o", &number);
 			break;
 		case 'x':
 		case 'X':
 			printf("Hexadezimal-Zahl eingeben: ");
-			scanf_s("%x", &number);
-			getchar();
+			fgets(string, sizeof(string), stdin);
+			sscanf(string, "%x", &number);
 			break;
 		case 'd':
 		case 'D':
 			printf("Dezimal-Zahl eingeben: ");
-			scanf_s("%d", &number);
-			getchar();
+			fgets(string, sizeof(string), stdin);
+			sscanf(string, "%d", &number);
 			break;
 		default:
 			fprintf(stderr, "Unknown output-type: '%c'!", outputType);
-			getchar();
 			return -1;
 	}
 	printf("Der eingegebene Wert in dezimalschreibweise lautet: %d\n", number);
