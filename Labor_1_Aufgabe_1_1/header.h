d6e2119f865f49982907983a73478ba2055728b9
Andreas Baulig <andreas.baulig@gmail.com>
2018-11-22 10:07:19 +0100

    Complete Labor_1_Aufgabe_1_1

---
+++ Labor_1_Aufgabe_1_1/header.h
@@ -0,0 +1,17 @@
+/* Uebungen Informatik 1, Aufgabe 1.1
+Erstersteller: Ralf Boeckle
+Datum:24.03.98
+Version: 01
+Change History:
+---------------
+Aenderungsgrund durchgefuehrte Aenderung Autor Datum
+--------------------------------------------------------
+Programmbeschreibung:
+---------------------
+Eingabeparameter:
+-----------------------------
+Ausgabeparameter:
+-----------------------------
+Fehlerausgänge:
+---------------
+*/
