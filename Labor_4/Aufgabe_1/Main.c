b95b7ce939ef933855d8fcbbb2488d3b40b0352e
Andreas Baulig <andreas.baulig@gmail.com>
2019-01-10 09:58:15 +0100

    Complete Labor_4/Aufgabe_1

---
+++ Labor_4/Aufgabe_1/Main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int n = 0;
+
+bool eingabe(void) {
+    printf("Input value to factorialize: ");
+    char buffer[128];
+    fgets(buffer, sizeof(buffer), stdin);
+    int consumed = sscanf(buffer, "%d", &n);
+    return consumed == 1;
+}
+
+void ausgabe(unsigned long factorial) {
+    printf("!n = %lu\n", factorial);
+}
+
+unsigned long faku(int n) {
+    if (n <= 0) {
+        return 1;
+    }
+    unsigned long sub = faku(n-1);
+    return n * sub;
+}
+
+int main(void) {
+    bool validInput = false;
+    do {
+        validInput = eingabe();
+    } while (!validInput);
+
+    unsigned long factorial = faku(n);
+
+    ausgabe(factorial);
+
+    return 0;
+}
